a7eb5b717d42d7b14cf0dd36a24f91a25b22080e
Sam Coe <coe6@purdue.edu>
2018-12-18 23:31:53 -0500

    Basic frame of webpage layed out

--- src/App.js
+++ src/App.js
@@ -6,9 +6,48 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-            
+
+        <header class="container">
+          <h1 class="col-lg-4">Samantha Coe</h1>
         </header>
+
+        <div class="row">
+          <h3 class="col-lg-3">info</h3>
+          <h3 class="col-sm-6">Hello</h3>
+        </div>
+
+        <div class="container">
+          <h3 class="col-lg-3">projects</h3>
+          <h3 class="col-sm-6">ksd</h3>
+        </div>
+      
+        <footer class="container">
+          <div class="row">
+            <p class="col-sm-4">&copy; Samantha Coe</p>
+            <ul class="col-sm-8">
+                <li class="col-sm-1">
+                  <a href="mailto:coe6@purdue.edu" target="_blank">
+                    <img src="assets/email.png"/>
+                  </a>
+                </li>
+                <li class="col-sm-1">
+                  <a href="https://github.com/coe6" target="_blank">
+                    <img src="assets/github.png"/>
+                  </a>
+                </li>
+                <li class="col-sm-1">
+                  <a href="https://www.linkedin.com/in/samantha-coe/" target="_blank">
+                    <img src="assets/linkedin.png"/>
+                  </a>
+                </li>
+                <li class="col-sm-1">
+                  <a href="/SamCoe_resume.pdf" target="_blank">
+                    <img src="assets/document.png"/>
+                  </a>
+                </li>
+              </ul>
+          </div>
+        </footer>
       </div>
     );
   }
