8991fe93e1820de9a29bb40191fe3f2a6031bf65
Sam Coe <coe6@purdue.edu>
2018-12-18 22:39:58 -0500

    Added footer

--- src/App.js
+++ src/App.js
@@ -7,13 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-            <header>Samantha Coe</header>
-
-            <p>Hello! I am currently a sophomore at Purdue University in West Lafayette, Indiana. 
-              Here, I am studying Computer Science with a focus in Machine Intelligence and as well as minoring in French.
-              
-              Please feel free to reach out to me!!
-              </p>
+            
         </header>
       </div>
     );
