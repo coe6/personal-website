da69086b6ec9ca8577f8bde864e77ab8110d1a3c
Sam Coe <coe6@purdue.edu>
2018-12-23 03:45:08 -0500

    update website layout

--- src/App.js
+++ src/App.js
@@ -9,47 +9,58 @@ class App extends Component {
 
         <header class="container">
           <h1 class="col-lg-12">Samantha Coe</h1>
-            
         </header>
 
         <div id="hLINKS" class="col-lg-12">
           <ul id="hLINKS" class="col-sm-8">
             <li id="hLINKS" class="col-sm-1">
-              <a href="mailto:coe6@purdue.edu">
-                <img src="assets/email.png"/>
+              <a href="mailto:coe6@purdue.edu" class="fa fa-envelope">
               </a>
             </li>
             <li class="col-sm-1">
-              <a href="https://github.com/coe6" target="_blank">
-                <img src="assets/github.png"/>
+              <a href="https://github.com/coe6" target="_blank" class="fa fa-github">
               </a>
             </li>
             <li class="col-sm-1">
-              <a href="https://www.linkedin.com/in/samantha-coe/" target="_blank">
-                <img src="assets/linkedin.png"/>
+              <a href="https://www.linkedin.com/in/samantha-coe/" target="_blank" class="fa fa-linkedin-square">
               </a>
             </li>
             <li class="col-sm-1">
-              <a href="/assets/SamCoe_resume.pdf" target="_blank">
-                <img src="assets/document.png"/>
+                <a href="https://www.instagram.com/sam.coee/" target="_blank" class="fa fa-instagram">
+                </a>
+              </li>
+            <li class="col-sm-1">
+              <a href="/assets/SamCoe_resume.pdf" target="_blank" class="fa fa-file-text">
               </a>
             </li>
           </ul>
         </div>
+
+        <div class="scroll row">
+          <a class="scroll" href="#info">info</a>
+          <a class="scroll"> | </a>
+          <a class="scroll" href="#projects">projects</a>
+        </div>
         
 
-        <div class="container">
-          <h4 id="label" class="col-lg-3">info</h4>
-        </div>
-        <div class="container">
+        <div class="row">
+          <a name="info">
+            <h4 id="label" class="col-lg-3">info</h4>
+          </a>
           <h5 id="description" class="col-sm-6">Hello! I am a sophomore at Purdue University studying Computer Science with a minor in French.
             I have an interest in full-stack development but I have a natural curiousity for learning anything new! Please do not
             hesitate to contact me!
           </h5>
         </div>
 
-        <div class="container">
-          <h4 id="label" class="col-lg-3">projects</h4>
+        <img id="meIMG" src="/assets/me2.jpg"></img>
+
+
+
+        <div class="fix container">
+          <a name="projects">
+            <h4 id="label" class="col-lg-3">projects</h4>
+          </a>
 
           <div class="container">
             <h5 id="description" class="col-sm-6"> <a href="https://github.com/coe6/spark-app/tree/master" target="_blank">SPARK!</a> is an iOS app designed to test the user's ability to memorize a sequence of colors.
@@ -70,27 +81,28 @@ class App extends Component {
           <div class="row">
             <p class="col-sm-4">&copy; Samantha Coe</p>
             <ul class="col-sm-8">
-                <li class="col-sm-1">
-                  <a href="mailto:coe6@purdue.edu" target="_blank">
-                    <img src="assets/email.png"/>
-                  </a>
-                </li>
-                <li class="col-sm-1">
-                  <a href="https://github.com/coe6" target="_blank">
-                    <img src="assets/github.png"/>
-                  </a>
-                </li>
-                <li class="col-sm-1">
-                  <a href="https://www.linkedin.com/in/samantha-coe/" target="_blank">
-                    <img src="assets/linkedin.png"/>
-                  </a>
-                </li>
-                <li class="col-sm-1">
-                  <a href="/assets/SamCoe_resume.pdf" target="_blank">
-                    <img src="assets/document.png"/>
-                  </a>
-                </li>
-              </ul>
+              <li id="hLINKS" class="col-sm-1">
+                <a href="mailto:coe6@purdue.edu" class="fa fa-envelope">
+                </a>
+              </li>
+              <li class="col-sm-1">
+                <a href="https://github.com/coe6" target="_blank" class="fa fa-github">
+                </a>
+              </li>
+              <li class="col-sm-1">
+                <a href="https://www.linkedin.com/in/samantha-coe/" target="_blank" class="fa fa-linkedin-square">
+                </a>
+              </li>
+              <li class="col-sm-1">
+                <a href="https://www.instagram.com/sam.coee/" target="_blank" class="fa fa-instagram">
+                </a>
+              </li>
+              <li class="col-sm-1">
+                <a href="/assets/SamCoe_resume.pdf" target="_blank" class="fa fa-file-text">
+                </a>
+              </li>
+              
+            </ul>
           </div>
         </footer>
       </div>
