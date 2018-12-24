2ecd13066ac3fdc1cb53577d3aacb08cc2d45d44
Sam Coe <coe6@purdue.edu>
2018-12-24 01:10:57 -0500

    Created project accordions

--- src/App.js
+++ src/App.js
@@ -6,10 +6,27 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+      
+      <div class="scroll col-lg-12">
+        <ul class="scroll col-sm-8">
+            <li class="col-sm-1">
+              <a class="scroll" href="#top">home</a>
+            </li>
+            <li class="col-sm-1">
+              <a class="scroll" href="#info">info</a>
+            </li>
+            <li class="col-sm-1">
+              <a class="scroll" href="#projects">projects</a>
+            </li>
+          </ul>
+      </div>
 
-        <header class="container">
-          <h1 class="col-lg-12">Samantha Coe</h1>
-        </header>
+
+      <a name="top"></a>
+      <header class="container">
+      
+        <h1 class="col-lg-12">Samantha Coe</h1>
+      </header>
 
         <div id="hLINKS" class="col-lg-12">
           <ul id="hLINKS" class="col-sm-8">
@@ -36,46 +53,72 @@ class App extends Component {
           </ul>
         </div>
 
-        <div class="scroll row">
-          <a class="scroll" href="#info">info</a>
-          <a class="scroll"> | </a>
-          <a class="scroll" href="#projects">projects</a>
-        </div>
-        
-
+      
         <div class="row">
-          <a name="info">
-            <h4 id="label" class="col-lg-3">info</h4>
-          </a>
-          <h5 id="description" class="col-sm-6">Hello! I am a sophomore at Purdue University studying Computer Science with a minor in French.
-            I have an interest in full-stack development but I have a natural curiousity for learning anything new! Please do not
-            hesitate to contact me!
-          </h5>
+            <h4 class="col-lg-4" id="label">info</h4>
+            <a name="info" class="col-lg-6"></a>
+            <h5 id="description" class="col-sm-4">Hello! I am a sophomore at Purdue University studying Computer Science with a minor in French.
+            I have an interest in full-stack development but I have a natural curiousity for learning anything new! Outside of coding, I have a passion
+            for the arts, including design, fashion, and music.
+            Please do not hesitate to contact me!
+            </h5>
         </div>
 
-        <img id="meIMG" src="/assets/me2.jpg"></img>
+        <img id="meIMG" src="/assets/me3.png"></img>
 
 
 
-        <div class="fix container">
-          <a name="projects">
-            <h4 id="label" class="col-lg-3">projects</h4>
-          </a>
+      <div class="panel-group" id="accordion">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h4 class="panel-title">
+              <a data-toggle="collapse" data-parent="#accordion" href="#spark">
+              <img class="projects" src="https://media.giphy.com/media/iO7HX7uu2totLccFfx/giphy.gif"/>
+              </a>
+            </h4>
+          </div>
+          <div id="spark" class="panel-collapse collapse">
+            <div class="panel-body"><a href="https://github.com/coe6/spark-app/tree/master" target="_blank">SPARK!</a> is an iOS app designed to test the user's ability to memorize a sequence of colors.
+              The game will display a sequence of colors and it is up to the user to recreate this pattern in order to continue to the next level. With each progressing level,
+              the game will build upon the previous pattern.</div>
+          </div>
+          </div>
 
-          <div class="container">
-            <h5 id="description" class="col-sm-6"> <a href="https://github.com/coe6/spark-app/tree/master" target="_blank">SPARK!</a> is an iOS app designed to test the user's ability to memorize a sequence of colors.
-            The game will display a sequence of colors and it is up to the user to recreate this pattern in order to continue to the next level. With each progressing level,
-            the game will build upon the previous pattern.
-            </h5>
-            <img class="projects" src="https://media.giphy.com/media/iO7HX7uu2totLccFfx/giphy.gif"/>
-
-            <h5 id="description" class="col-sm-12"><a href="https://github.com/coe6/Pic-a-Puppy-GUS-" target="_blank">Pic-A-Puppy</a> is an iOS app solely created for the purpose of displaying photos of my dog, Gus.
-            The user is able to heart their favorite images and later view them in a separate album. The app will display the images in a random order every time the full photo album is opened.
-            </h5>
-            <img class="projects" src="https://media.giphy.com/media/2WdEk8ApcKohbl5pnS/giphy.gif"/>
+          <div class="panel panel-default">
+          <div class="panel-heading">
+            <h4 class="panel-title">
+              <a data-toggle="collapse" data-parent="#accordion" href="#picPuppy">
+                <img class="projects" src="https://media.giphy.com/media/2WdEk8ApcKohbl5pnS/giphy.gif"/>
+              </a>
+            </h4>
+          </div>
+          <div id="picPuppy" class="panel-collapse collapse">
+            <div class="panel-body"><a href="https://github.com/coe6/Pic-a-Puppy-GUS-" target="_blank">Pic-A-Puppy</a> is an iOS app solely created for the purpose of displaying photos of my dog, Gus.
+              The user is able to heart their favorite images and later view them in a separate album. The app will display the images in a random order every time the full photo album is opened.</div>
+          </div>
           </div>
 
         </div>
+
+        {/* <div class="container">
+          <button class="col-lg-6" type="button" data-toggle="collapse" data-target="#spark"><img class="projects" src="https://media.giphy.com/media/iO7HX7uu2totLccFfx/giphy.gif"/></button>
+            <div id="spark" class="collapse">
+              <p id="description" class="col-sm-6"> <a href="https://github.com/coe6/spark-app/tree/master" target="_blank">SPARK!</a> is an iOS app designed to test the user's ability to memorize a sequence of colors.
+              The game will display a sequence of colors and it is up to the user to recreate this pattern in order to continue to the next level. With each progressing level,
+              the game will build upon the previous pattern.
+              </p>
+            </div>
+
+          <button class="col-lg-6" type="button" data-toggle="collapse" data-target="#picPuppy"><img class="projects" src="https://media.giphy.com/media/2WdEk8ApcKohbl5pnS/giphy.gif"/></button>
+            <div id="picPuppy" class="collapse">
+              <p id="description" class="col-sm-12"><a href="https://github.com/coe6/Pic-a-Puppy-GUS-" target="_blank">Pic-A-Puppy</a> is an iOS app solely created for the purpose of displaying photos of my dog, Gus.
+              The user is able to heart their favorite images and later view them in a separate album. The app will display the images in a random order every time the full photo album is opened.
+              </p>
+            </div>
+
+        </div> */}
+
+
       
         <footer class="container">
           <div class="row">
